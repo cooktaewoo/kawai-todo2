c3d2e0321d53aaeded724770c28d066905ffa1b9
taewoo <cooksunghun@naver.com>
2018-08-11 20:12:39 +0900

    Finish basic functions

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
-import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView} from 'react-native';
 import ToDo from './ToDo';
+import { AppLoading } from "expo";
+import uuidv from 'uuid/v1';
+import React from 'react';
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   state = {
-    newToDo: ""
+    newToDo: "",
+    loadedToDos: false,
+    toDos: {}
+  }
+
+  componentDidMount = () => {
+    this._loadToDos();
   }
 
   render() {
-    const { newToDo } = this.state;
+    const { newToDo, loadedToDos, toDos } = this.state;
+    // console.log(toDos);
+    if (!loadedToDos) {
+      return <AppLoading />
+    }
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
@@ -24,9 +36,19 @@ export default class App extends React.Component {
             placeholderTextColor={"#999"}
             returnKeyType={"done"}
             autoCorrect={false}
+            onSubmitEditing={this._addToDo}
           />
           <ScrollView contentContainerStyle={styles.todos}>
-            <ToDo />
+           {/* ToDo 객체의 text props에 넘기기 */}
+           {/* todo.id 같은 유니크 키가 있어야 한다 ...toDO 로 다 넘기면 안됨 */}
+           {/* _deleteToDo 와 같이 함수를 넘길 수가 있음 */}
+            {Object.values(toDos).map(toDo => <ToDo 
+                key={toDo.id} {...toDo} 
+                deleteToDo={this._deleteToDo} 
+                completeToDo={this._completeToDo}
+                uncompleteToDo={this._uncompleteToDo}
+                updateToDo={this._updateTodo}
+            />)} 
           </ScrollView>
         </View>
       </View>
@@ -35,6 +57,107 @@ export default class App extends React.Component {
   _controlNewToDo = text => {
     this.setState({
       newToDo: text
+    })
+  }
+
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true
+    });
+  }
+
+  _addToDo = () => {
+    const { newToDo } = this.state;
+    if (newToDo !== "") {
+      
+      this.setState(prevState => {
+        const ID = uuidv();
+        const newToDoObject = {
+          [ID] : {
+            id : ID,
+            isCompleted: false,
+            text: newToDo,
+            createdAt: Date.now()
+          }
+        };
+        // ... 써서 Rest es6 문법
+        const newState = {
+          ...prevState,
+          newToDo: "", // Input 값 초기화
+          toDos : { // todo list 추가
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        }
+        return { ...newState };
+
+      });
+
+    }
+  }
+
+  _deleteToDo = (id) => {
+      this.setState(prevState => {
+        const toDos = prevState.toDos;
+        delete toDos[id];
+
+        const newState = {
+          ...prevState,
+          ...toDos
+        }
+        return { ...newState };
+
+      });
+  }
+
+  _completeToDo = (id) => {
+    this.setState(prevState => {
+      const toDos = prevState.toDos;
+      const newState = {
+        ...prevState,
+        toDos : { // todo list 추가
+          ...prevState.toDos,
+          [id] : {
+            ...prevState.toDos[id],
+            isCompleted : true
+          }
+        }
+      }
+      return { ...newState };
+    })
+  }
+
+  _uncompleteToDo = (id) => {
+    this.setState(prevState => {
+      const toDos = prevState.toDos;
+      const newState = {
+        ...prevState,
+        toDos : { // todo list 추가
+          ...prevState.toDos,
+          [id] : {
+            ...prevState.toDos[id],
+            isCompleted : false
+          }
+        }
+      }
+      return { ...newState };
+    })
+  }
+
+  _updateTodo = (id, text) => {
+    this.setState(prevState => {
+      const toDos = prevState.toDos;
+      const newState = {
+        ...prevState,
+        toDos : { // todo list 추가
+          ...prevState.toDos,
+          [id] : {
+            ...prevState.toDos[id],
+            text : text
+          }
+        }
+      }
+      return { ...newState };
     })
   }
 }
--- ToDo.js
+++ ToDo.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native"
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from "react-native"
+import PropTypes from "prop-types";
+//prop-types : props에 대해서 런타임 환경(개발 환경 only)에서 타입체크를 해주는 React의 내장된 기능이다
 
 const { height, width } = Dimensions.get("window");
 
 //state 수정이 필요하기 때문에 stateful component 를 사용한다
-export default class ToDo extends Component {    
+export default class ToDo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing : false,
+            toDoValue : props.text 
+        };
+    }
+
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+        isCompleted: PropTypes.bool.isRequired,
+        deleteToDo : PropTypes.func.isRequired,
+        id: PropTypes.string.isRequired,
+        completeToDo : PropTypes.func.isRequired,
+        uncompleteToDo : PropTypes.func.isRequired,
+        updateToDo : PropTypes.func.isRequired
+    }
+    
     state = {
         isEditing : false,
-        isCompleted : false
+        toDoValue : ''
     };
 
     render() {
-        const { isCompleted, isEditing } = this.state;
+        const { isEditing, toDoValue } = this.state;
+        const { text, id, deleteToDo, isCompleted } = this.props;
+
         return (
             <View style={styles.container}>
                 <View style={styles.column}>
@@ -23,10 +45,23 @@ export default class ToDo extends Component {
                             ]}>
                         </View>
                     </TouchableOpacity>
-                        <Text style={[
-                            styles.text,
-                            isCompleted ? styles.completedText : styles.uncompletedText
-                        ]}> Hello I am a To Do</Text>
+                        {isEditing ? (
+                            <TextInput 
+                                style={[
+                                    styles.text,
+                                    styles.input,
+                                    isCompleted ? styles.completedText : styles.uncompletedText
+                                ]} 
+                                value={toDoValue}
+                                multiline={true}
+                                onChangeText={this._controlInput}>
+                            </TextInput>
+                        ) : (
+                            <Text style={[
+                                styles.text,
+                                isCompleted ? styles.completedText : styles.uncompletedText
+                            ]}> {text} </Text>
+                        )}
                 </View>
                 {isEditing ? (
                     <View style={styles.actions}>
@@ -43,7 +78,7 @@ export default class ToDo extends Component {
                                 <Text style={styles.actionText}>✏️</Text>
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={this._deleteRow}>
+                        <TouchableOpacity onPress={() => deleteToDo(id)}>
                             <View style={styles.actionContainer}>
                                 <Text style={styles.actionText}>❌</Text>
                             </View>
@@ -55,28 +90,32 @@ export default class ToDo extends Component {
     }
 
     _toggleComplete = () => {
-        this.setState(prevState => {
-            return {
-                isCompleted : !prevState.isCompleted
-            }
-        });
+        const { isCompleted, completeToDo, uncompleteToDo, id } = this.props;
+        if (isCompleted) {
+            uncompleteToDo(id);
+        } else {
+            completeToDo(id);
+        }
     };
 
     _startEditing = () => {
         this.setState({
-            isEditing : true    
+            isEditing : true
         });
     };
 
     _completeEditing = () => {
+        const { toDoValue } = this.state;
+        const { id, updateToDo } = this.props;
+        updateToDo(id, toDoValue)
+        this.setState({isEditing: false}) // 아이콘 변경을 위해서
+    };
+
+    _controlInput = (text) => {
         this.setState({
-            isEditing : false    
+            toDoValue : text
         });
-    };
-
-    _deleteRow = () => {
-
-    };
+    }
 }
 
 const styles = StyleSheet.create({
@@ -120,7 +159,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems : 'center',
         width : width / 2,
-        justifyContent : 'space-between'
     },
     actions : {
         flexDirection : 'row'
@@ -128,5 +166,10 @@ const styles = StyleSheet.create({
     actionContainer : {
         marginVertical : 10,
         marginHorizontal: 10
+    },
+    input : {
+        marginVertical : 15,
+        width : width / 2,
+        paddingBottom : 5
     }
 });
